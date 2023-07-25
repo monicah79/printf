b2d6925ff4b733e227cb3b0a7bdcd77451253300
monicah79 <monniewanjema@gmail.com>
2023-07-24 19:50:12 -0700

    Updates

--- _printf.c
+++ _printf.c
@@ -1,56 +1,111 @@
 #include <stdarg.h>
 #include <stdio.h>
-#include "main.h"
+#include "printf.h"
 
-/**
- * _printf - Prints a string
- * @format: List a of arguments
- * Return: Number of chars printed
- */
+int handle_char(va_list args, int count)
+{
+    count += printf("%c", va_arg(args, int));
+    return count;
+}
+
+int handle_string(va_list args, int count)
+{
+    count += printf("%s", va_arg(args, char *));
+    return count;
+}
+
+int handle_decimal(va_list args, int count)
+{
+    char buffer[12];
+    int num = va_arg(args, int);
+    int len = sprintf(buffer, "%d", num);
+    count += printf("%.*s", len, buffer);
+    return count;
+}
+
+int handle_unsigned(va_list args, int count)
+{
+    count += printf("[%u]", va_arg(args, unsigned int));
+    return count;
+}
+
+int handle_octal(va_list args, int count)
+{
+    count += printf("[%o]", va_arg(args, unsigned int));
+    return count;
+}
+
+int handle_hex(va_list args, int count, char format)
+{
+    unsigned int num = va_arg(args, unsigned int);
+    if (format == 'x') {
+        count += printf("[%08x, %08X]", num, num);
+    }
+    return count;
+}
+
+int handle_pointer(va_list args, int count)
+{
+    count += printf("[%p]", va_arg(args, void *));
+    return count;
+}
+
+int handle_percent(int count)
+{
+    count += printf("%%");
+    return count;
+}
 
 int _printf(const char *format, ...)
 {
 	int count = 0;
-	int num;
-	va_list args;
-
-	va_start(args, format);
-
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 'c')
-				count += printf("%c", va_arg(args, int));
-			else if (*format == 's')
-				count += printf("%s", va_arg(args, char *));
-			else if (*format == 'd' || *format == 'i')
-			{
-				num = va_arg(args, int);
-				count += printf("[%d, %i]", num, num);
-			}
-			else if (*format == 'u')
-				count += printf("[%u]", va_arg(args, unsigned int));
-			else if (*format == 'o')
-				count += printf("[%o]", va_arg(args, unsigned int));
-			else if (*format == 'x' || *format == 'X')
-			{
-				unsigned int num = va_arg(args, unsigned int);
-
-				count += printf("[%x, %X]", num, num);
-			}
-			else if (*format == 'p')
-				count += printf("[%p]", va_arg(args, void *));
-			else if (*format == '%')
-				count += printf("%%");
-			else
-				count += printf("%%%c", *format);
-		}
-		else
-			count += printf("%c", *format);
-		format++;
-	}
-	va_end(args);
-	return (count);
+    va_list args;
+    va_start(args, format);
+    
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            switch (*format)
+            {
+                case 'c':
+                    count = handle_char(args, count);
+                    break;
+                case 's':
+                    count = handle_string(args, count);
+                    break;
+                case 'd':
+                case 'i':
+                    count = handle_decimal(args, count);
+                    break;
+                case 'u':
+                   count = handle_unsigned(args, count);
+                    break;
+                case 'o':
+                    count = handle_octal(args, count);
+                    break;
+                case 'x':
+                case 'X':
+                    count = handle_hex(args, count, *format);
+                    break;
+                case 'p':
+                    count = handle_pointer(args, count);
+                    break;
+                case '%':
+                    count = handle_percent(count);
+                    break;
+                default:
+                    count += printf("%%%c", *format);
+                    break;
+            }
+        }
+        else
+        {
+            count += printf("%c", *format);
+        }
+        format++;
+    }
+    va_end(args);
+    return count;
 }
